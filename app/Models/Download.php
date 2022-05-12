86b39d4375d57fd31410e7dbb9daa0865ee4cd53
Timchik14 <temurtheg@rambler.ru>
2022-05-12 16:08:42 +0500

    move logic from models and command to services
    moved logic from models, used static method

--- app/Models/Download.php
+++ app/Models/Download.php
@@ -2,17 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Download extends Model
 {
-    use HasFactory;
-
-    protected $guarded = [
-
-    ];
+    public function getDownloads()
+    {
+        return Download::select('file_id', DB::raw('count(*) as total'))
+            ->groupBy('file_id')
+            ->get();
+    }
 
     public function log(File $file)
     {
@@ -22,24 +21,6 @@ class Download extends Model
             'group_id' => $file->group_id,
             'file_id' => $file->id,
         ]);
-    }
-
-    public function prepare(Collection $collection)
-    {
-        foreach ($collection as $item) {
-            $path = $item->path;
-            $item['format'] = substr($path, -3, 3);
-            $from = strripos($path, '/') + 1;
-            $item['name'] = substr($path, $from, strlen($path) - $from - 4);
-            $item['group_name'] = $item->group->name;
-        }
-        return $collection;
-    }
-
-    public function getWithRelations()
-    {
-        $data = Download::with(['user', 'group'])->get();
-        return $this->prepare($data);
     }
 
     public function user()
--- app/Models/TextToken.php
+++ app/Models/TextToken.php
@@ -2,17 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
 class TextToken extends Model
 {
-    use HasFactory;
-
-    protected $fillable = [
-        'token',
-    ];
-
     public function user()
     {
         return $this->belongsTo(User::class);
